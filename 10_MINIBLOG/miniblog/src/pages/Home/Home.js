bfdc6cdbe25296e2849fe475653f7d391c297f1a
Daniel Sanches <danielsanches6301@gmail.com>
2023-01-10 20:21:40 -0300

    ajustes na página home

--- 10_MINIBLOG/miniblog/src/pages/Home/Home.js
+++ 10_MINIBLOG/miniblog/src/pages/Home/Home.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 import styles from "./Home.module.css";
 
 const Home = () => {
+  const [query, setQuery] = useState("");
+  const [posts, setPosts] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <div>Home</div>
+    <div className={styles.home}>
+      <h1>Veja os nossos posts mais recentes</h1>
+      <form className={styles.searchForm}>
+        <input type="text" placeholder='Ou busque por tags...' onChange={(e) => setQuery(e.target.value)} />
+        <button className='btn btn-dark'>Pesquisar</button>
+      </form>
+
+      <div className="posts">
+        <h1>Posts...</h1>
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Não foram encontrados posts</p>
+            <Link className='btn' to={"/posts/create"}>Criar primeiro post</Link>
+          </div>
+        )}
+      </div>
+    </div>
   )
 }
 
