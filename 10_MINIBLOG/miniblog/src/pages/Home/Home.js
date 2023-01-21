9999deb8adc2aab303bd71ed4eb7fe35c736c81e
Daniel Sanches <danielsanches6301@gmail.com>
2023-01-21 14:26:30 -0300

    ajustes

--- 10_MINIBLOG/miniblog/src/pages/Home/Home.js
+++ 10_MINIBLOG/miniblog/src/pages/Home/Home.js
@@ -1,36 +1,58 @@
-import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { useState } from 'react';
-
+// CSS
 import styles from "./Home.module.css";
 
+// hooks
+import { useFetchDocument } from "../../hooks/useFetchDocument";
+import { useNavigate, Link } from "react-router-dom";
+
+// react
+import { useState } from "react";
+
+// components
+import PostDetail from "../../components/PostDetail";
+
 const Home = () => {
+  const { documents: posts, loading } = useFetchDocument("posts");
+
+  const navigate = useNavigate();
+
   const [query, setQuery] = useState("");
-  const [posts, setPosts] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-  }
+
+    if (query) {
+      return navigate(`/search?q=${query}`);
+    }
+  };
+
+  console.log(loading);
 
   return (
     <div className={styles.home}>
       <h1>Veja os nossos posts mais recentes</h1>
-      <form className={styles.searchForm}>
-        <input type="text" placeholder='Ou busque por tags...' onChange={(e) => setQuery(e.target.value)} />
-        <button className='btn btn-dark'>Pesquisar</button>
+      <form className={styles.search_form} onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Ou busque por tags..."
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="btn btn-dark">Pesquisar</button>
       </form>
-
-      <div className="posts">
-        <h1>Posts...</h1>
+      <div className="post-list">
+        {loading && <p>Carregando...</p>}
         {posts && posts.length === 0 && (
           <div className={styles.noposts}>
             <p>Não foram encontrados posts</p>
-            <Link className='btn' to={"/posts/create"}>Criar primeiro post</Link>
+            <Link to="/posts/create" className="btn">
+              Criar primeiro post
+            </Link>
           </div>
         )}
+        {posts && posts.map((post) => <PostDetail key={post.id} post={post} />)}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
