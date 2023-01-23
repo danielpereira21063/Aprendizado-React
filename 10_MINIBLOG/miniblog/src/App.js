94e2cede67eba8ef98cda1da88c4419e339c45e9
Daniel Sanches <danielsanches6301@gmail.com>
2023-01-23 09:02:11 -0300

    novos ajustes

--- 10_MINIBLOG/miniblog/src/App.js
+++ 10_MINIBLOG/miniblog/src/App.js
@@ -1,26 +1,35 @@
-import './App.css';
-import { BrowserRouter, Routes, Route, Navigate, Router } from 'react-router-dom';
-import { onAuthStateChanged } from 'firebase/auth';
-import { useState, useEffect } from 'react';
-import { useAuthentication } from './hooks/useAuthentication';
+import "./App.css";
 
-import { AuthProvider } from './context/AuthContext';
-import Footer from './components/Footer';
-import Navbar from './components/Navbar';
-import About from './pages/About/About';
-import Home from './pages/Home/Home';
-import Login from './pages/Login/Login';
-import Register from './pages/Register/Register';
-import CreatePost from './pages/CreatePost/CreatePost';
-import Dashboard from './pages/Dashboard/Dashboard';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
 
-//pages
+// hooks
+import { useState, useEffect } from "react";
+import { useAuthentication } from "./hooks/useAuthentication";
+
+// pages
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Post from "./pages/Post/Post";
+
+// components
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import CreatePost from "./pages/CreatePost/CreatePost";
+import Search from "./pages/Search/Search";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import EditPost from "./pages/EditPost/EditPost";
+
+// context
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
   const [user, setUser] = useState(undefined);
   const { auth } = useAuthentication();
 
-  const loadinUser = user === undefined;
+  const loadingUser = user === undefined;
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
@@ -28,7 +37,7 @@ function App() {
     });
   }, [auth]);
 
-  if (loadinUser) {
+  if (loadingUser) {
     return <p>Carregando...</p>;
   }
 
@@ -39,12 +48,30 @@ function App() {
           <Navbar />
           <div className="container">
             <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/about' element={<About />}></Route>
-              <Route path='/login' element={!user ? <Login /> : <Navigate to="/"></Navigate>}></Route>
-              <Route path='/register' element={!user ? <Register /> : <Navigate to="/"></Navigate>}></Route>
-              <Route path='/posts/create' element={user ? <CreatePost /> : <Navigate to="/login"></Navigate>}></Route>
-              <Route path='/dashboard' element={user ? <Dashboard /> : <Navigate to="/login"></Navigate>}></Route>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route
+                path="/posts/create"
+                element={user ? <CreatePost /> : <Navigate to="/login" />}
+              />
+              <Route
+                path="/posts/edit/:id"
+                element={user ? <EditPost /> : <Navigate to="/login" />}
+              />
+              <Route path="/posts/:id" element={<Post />} />
+              <Route path="/search" element={<Search />} />
+              <Route
+                path="/login"
+                element={!user ? <Login /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/register"
+                element={!user ? <Register /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/dashboard"
+                element={user ? <Dashboard /> : <Navigate to="/login" />}
+              />
             </Routes>
           </div>
           <Footer />
--- 10_MINIBLOG/miniblog/src/components/Footer.js
+++ 10_MINIBLOG/miniblog/src/components/Footer.js
@@ -1,13 +1,12 @@
-import React from 'react';
 import styles from "./Footer.module.css";
 
 const Footer = () => {
-    return (
-        <footer className={styles.footer}>
-            <h3>Escreva sobre o que você tem enteresse</h3>
-            <p>Mini Blog &copy; {new Date().getFullYear()}</p>
-        </footer>
-    )
-}
+  return (
+    <footer className={styles.footer}>
+      <h3>Escreva sobre o que você tem interesse!</h3>
+      <p>Mini Blog &copy; 2022</p>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
