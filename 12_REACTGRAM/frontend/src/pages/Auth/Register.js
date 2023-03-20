3edbcc4f8ab6fbf11feb6bf28f082a351da7efd2
Daniel Sanches <danielsanches6301@gmail.com>
2023-03-20 14:44:21 -0300

    commit

---
+++ 12_REACTGRAM/frontend/src/pages/Auth/Register.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
+import "./Auth.css"
+
+import { register, reset } from '../../slices/authSlice';
+import { useSelector, useDispatch } from 'react-redux';
+
+const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const dispach = useDispatch();
+  const { loading, error } = useSelector((state) => state.auth);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const user = {
+      name,
+      email,
+      password,
+      confirmPassword
+    }
+
+    dispach(register(user));
+  };
+
+  useEffect(() => {
+    dispach(reset());
+  }, [dispach]);
+  
+  return (
+    <div id='register'>
+      <h2>ReactGram</h2>
+      <p className='subtitlel'>Cadastre-se para ver as fotos dos seus amigos.</p>
+
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Nome" onChange={(e) => setName(e.target.value)} value={name || ""} />
+        <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} value={email || ""} />
+        <input type="password" placeholder="Senha" onChange={(e) => setPassword(e.target.value)} value={password || ""} />
+        <input type="password" placeholder="Confirme a senha" onChange={(e) => setConfirmPassword(e.target.value)} value={confirmPassword || ""} />
+        <input type="submit" value="Cadastrar" />
+      </form>
+
+      <p>
+        Já tem conta? <Link to={"/login"}>Clique aqui.</Link>
+      </p>
+    </div>
+  )
+}
+
+export default Register;
